822b5292e98dcdd9f963d2e5a2e42cf681d8a54c
srod0010 <saviofrod@gmail.com>
2019-01-01 15:55:19 -0800

    Update passport to return payload

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,15 @@ options.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
     passport.use(new JwtStrategy(options, (jwt_payload, done) => {
-        // This payload includes the items we specified earlier
-        console.log(jwt_payload);
+        User.findById(jwt_payload.id)
+            .then(user => {
+                if (user) {
+                    // return the user to the frontend
+                    return done(null, user);
+                }
+                // return false since there is no user
+                return done(null, false);
+            })
+            .catch(err => console.log(err));
     }));
 };
