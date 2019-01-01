665d920bc449facbc938e17cbfb6c4648443d774
srod0010 <saviofrod@gmail.com>
2019-01-01 14:27:13 -0800

    Setup route to register users

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,4 +7,36 @@ router.get("/test", (req, res) => res.json({
     msg: "This is the users route"
 }));
 
+router.post('/register', (req, res) => {
+    // Check to make sure nobody has already registered with a duplicate email
+    User.findOne({
+            email: req.body.email
+        })
+        .then(user => {
+            if (user) {
+                // Throw a 400 error if the email address already exists
+                return res.status(400).json({
+                    email: "A user has already registered with this address"
+                })
+            } else {
+                // Otherwise create a new user
+                const newUser = new User({
+                    handle: req.body.handle,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
+                })
+            }
+        })
+})
+
 module.exports = router;
