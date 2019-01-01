b31dbfbf56a9ff0ceb15d50676facfb36dc41b02
srod0010 <saviofrod@gmail.com>
2019-01-01 15:10:03 -0800

    Add route for login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
@@ -8,6 +9,7 @@ router.get("/test", (req, res) => res.json({
     msg: "This is the users route"
 }));
 
+// check post
 router.post('/register', (req, res) => {
     // Check to make sure nobody has already registered with a duplicate email
     User.findOne({
@@ -40,33 +42,48 @@ router.post('/register', (req, res) => {
         })
 })
 
-router.post('/login', (req, res) => {
-    const email = req.body.email;
+router.post("/login", (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const name = req.body.name;
     const password = req.body.password;
 
     User.findOne({
-            email
-        })
-        .then(user => {
-            if (!user) {
-                return res.status(404).json({
-                    email: 'This user does not exist'
-                });
-            }
+        name
+    }).then(user => {
+        if (!user) {
+            errors.name = "This user does not exist";
+            return res.status(400).json(errors);
+        }
 
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if (isMatch) {
-                        res.json({
-                            msg: 'Success'
-                        });
-                    } else {
-                        return res.status(400).json({
-                            password: 'Incorrect password'
-                        });
-                    }
-                })
-        })
-})
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+
+                jwt.sign(payload, keys.secretOrKeys, {
+                    expiresIn: 3600
+                }, (err, token) => {
+                    res.json({
+                        success: true,
+                        token: "Bearer " + token
+                    });
+                });
+            } else {
+                errors.password = "Incorrect password";
+                return res.status(400).json(errors);
+            }
+        });
+    });
+});
 
 module.exports = router;
