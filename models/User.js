bd5dfeb52a956b048a4d9840bbaeec2f8ddd7a61
srod0010 <saviofrod@gmail.com>
2019-01-01 14:18:35 -0800

    Setup User schema

--- models/User.js
+++ models/User.js
@@ -1,3 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const UserSchema = new Schema({
+    handle: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
