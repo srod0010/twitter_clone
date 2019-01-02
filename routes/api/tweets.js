08ff7b636ef9a66d2cb11e340d96f7aec0d09629
srod0010 <saviofrod@gmail.com>
2019-01-01 17:46:24 -0800

    Add Post route for tweets

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const validateTweetInput = require("../../validation/tweets");
+const Tweet = require("../../models/Tweet");
 
 router.get("/test", (req, res) => res.json({
     msg: "This is the tweets route"
 }));
 
+router.post("/", passport.authenticate("jwt",{session: false}),
+    (req,res) => {
+        const { isValid, errors } = validateTweetInput(req.body);
+
+        if(!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        const newTweet = new Tweet({
+            user: req.user.id,
+            text: req.body.text
+        });
+
+        newTweet
+            .save()
+            .then(tweet => res.json(tweet));
+    }
+)
 module.exports = router;
