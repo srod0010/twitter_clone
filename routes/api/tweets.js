b02cffab2d5b33fa90b5d8e66657b533393b4c6e
srod0010 <saviofrod@gmail.com>
2019-01-01 17:48:57 -0800

    Add Get routes for tweets

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -8,6 +8,33 @@ router.get("/test", (req, res) => res.json({
     msg: "This is the tweets route"
 }));
 
+// GET all tweets
+router.get('/', (req, res) => {
+    Tweet.find()
+        .sort({ date: -1 })
+        .then(tweets => res.json(tweets))
+        .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
+});
+
+// GET single user tweets
+router.get('/user/:user_id', (req, res) => {
+    Tweet.find({user: req.params.user_id})
+        .then(tweets => res.json(tweets))
+        .catch(err =>
+            res.status(404).json({ notweetsfound: 'No tweets found from that user' }
+        )
+    );
+});
+
+// GET individual tweet
+router.get('/:id', (req, res) => {
+    Tweet.findById(req.params.id)
+        .then(tweet => res.json(tweet))
+        .catch(err =>
+            res.status(404).json({ notweetfound: 'No tweet found with that ID' })
+        );
+});
+
 router.post("/", passport.authenticate("jwt",{session: false}),
     (req,res) => {
         const { isValid, errors } = validateTweetInput(req.body);
